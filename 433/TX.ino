e6b9c3b1e38a5ca00dc8444de62447ed42bfa1d5
VIKISHAUL <82369344+VIKISHAUL@users.noreply.github.com>
2021-04-12 11:08:47 +0300

    Create TX.ino

---
+++ 433/TX.ino
@@ -0,0 +1,34 @@
+#include <VirtualWire.h>
+const int tx_pin = 2;
+const int indicate_pin = 13;
+
+#define potpin A0
+int potval = 0;
+void setup() {
+  Serial.begin(9600);
+  pinMode(potpin, INPUT);
+  vw_set_tx_pin(tx_pin);
+  pinMode(indicate_pin, OUTPUT);
+  vw_setup(2000);
+}
+
+void loop() {
+  potval = analogRead(potpin);
+  if (potval <= 500) {
+    digitalWrite(indicate_pin, LOW);
+    Serial.print("potval = ");
+    Serial.println(potval);
+    send("a");
+  }
+  else {
+    digitalWrite(indicate_pin, HIGH);
+    Serial.print("potval = ");
+    Serial.println(potval);
+    send("b");
+  }
+}
+void send (char *message)
+{
+  vw_send((uint8_t *)message, strlen(message));
+  vw_wait_tx();
+}
